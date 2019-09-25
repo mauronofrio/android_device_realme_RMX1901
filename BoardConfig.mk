b2126d056f43900a205fd0849847dc013c78b9e0
mauronofrio matarrese <poronofrio@gmail.com>
2019-09-25 14:00:15 +0200

    Adding EDL button in reboot menu

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
 TW_NO_SCREEN_TIMEOUT := true
+TW_HAS_EDL_MODE := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
